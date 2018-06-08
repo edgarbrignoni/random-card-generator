5652db6d044b2ab16414b1f3cec6650fe9d97423
Edgar Brignoni <edgarbrignoni@icloud.com>
2018-06-08 13:51:14 +0000

    fix card colors

--- index.js
+++ index.js
@@ -1,33 +1,39 @@
 function init() {
-	var randomNumber = Math.floor(Math.random() * 12);
+	var randomNumber = Math.floor(Math.random() * 12 + 1);
 	var randomSuit = Math.floor(Math.random() * 4);
 	buildCard(randomNumber, randomSuit);
 }
 
 function buildCard(number, suit) {
-	var aux = number;
 	var top = document.querySelector('.top');
 	var bot = document.querySelector('.bot');
+	console.log(number + ' ' + suit);
+	
+	if (number == 1){ number = 'A'};
+	if (number == 11){ number = 'J'};
+	if (number == 12){ number = 'Q'};
+	if (number == 13){ number = 'K'};
+	
 	
 	if (suit == 0) {
 		top.setAttribute('class', 'top heart');
-		document.querySelector("mid").style.color = "red";
-		document.querySelector("mid").innerHTML = aux;
 		bot.setAttribute('class', 'bot heart');
+		document.querySelector('.mid').innerHTML = number;
+		document.querySelector('.mid').style.color = 'red';
 	} else if (suit == 1) {
 		top.setAttribute('class', 'top spade');
-		document.querySelector("mid").style.color = "black";
-		document.querySelector("mid").innerHTML = aux;
 		bot.setAttribute('class', 'bot spade');
+		document.querySelector('.mid').innerHTML = number;
+		document.querySelector('.mid').style.color = 'black';
 	} else if (suit == 2) {
 		top.setAttribute('class', 'top diamo');
-		document.querySelector("mid").style.color = "red";
-		document.querySelector("mid").innerHTML = aux;
 		bot.setAttribute('class', 'bot diamo');
+		document.querySelector('.mid').innerHTML = number;
+		document.querySelector('.mid').style.color = 'red';
 	} else if (suit == 3) {
 		top.setAttribute('class', 'top clubs');
-		document.querySelector("mid").style.color = "black";
-		document.querySelector("mid").innerHTML = aux;
 		bot.setAttribute('class', 'bot clubs');
+		document.querySelector('.mid').innerHTML = number;
+		document.querySelector('.mid').style.color = 'black';
 	}
 }
